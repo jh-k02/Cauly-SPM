25bc60636fb7c93b88408fb5ac1a04083cbf2e9d
jh-k02 <76684103+jh-k02@users.noreply.github.com>
2026-01-08 15:06:16 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 5.8
+import PackageDescription
+
+let package = Package(
+    name: "Cauly-SPM",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "CaulySDK",
+            targets: ["Cauly-SPM"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CaulySDK",
+            path: "CaulySDK.xcframework"
+        ),
+
+        .target(
+            name: "Cauly-SPM",
+            dependencies: [
+                .target(name: "CaulySDK")
+            ],
+            linkerSettings: [
+                .linkedFramework("AVKit"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("MediaPlayer"),
+                .linkedFramework("CFNetwork"),
+                .linkedFramework("EventKit"),
+                .linkedFramework("AdSupport"),
+                .linkedFramework("MessageUI",),
+            ]
+        )
+    ]
+)
